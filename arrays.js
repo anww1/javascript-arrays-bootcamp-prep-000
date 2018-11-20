b9c1350704ec23f6299cccf334b4ed2792cedf90
Alexandria Walker-Wade <alexandriawalkerwade@gmail.com>
2018-11-20 02:47:45 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,73 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+function addElementToBeginningOfArray(array, element)
+{
+array= [element,...array];
+return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element)
+{
+ array.unshift(element);
+ return array;
+}
+
+function addElementToEndOfArray(array,element)
+{
+array=[...array,element];
+return array;
+}
+
+function destructivelyAddElementToEndOfArray(array,element)
+{
+ array.push(element);
+ return array;  
+}
+
+function accessElementInArray(array,index)
+{
+
+
+return    
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+ array.shift();
+  return array;
+}
+
+
+function removeElementFromBeginningOfArray(array)
+{
+ array.
+}
+
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  
+}
+
+function removeElementFromEndOfArray(array)
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
