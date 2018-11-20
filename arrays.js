d18f5bc4ea7a444a6ed6f0bf603b5d905381cd75
Alexandria Walker-Wade <alexandriawalkerwade@gmail.com>
2018-11-20 03:05:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,12 +39,14 @@ function destructivelyRemoveElementFromBeginningOfArray(array)
 
 function removeElementFromBeginningOfArray(array)
 {
-
+  array.slice(1);
+  return array;
 }
 
 
 function destructivelyRemoveElementFromEndOfArray(array){
   
+  return array;
 }
 
 function removeElementFromEndOfArray(array)
